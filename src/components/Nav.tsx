2e2993cb7a923b567dd5da0cb1d8810cd0d24441
yake go <cazser>
2020-10-10 15:32:20 +0800

    搞定三个icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import x from "../icons/money.svg";
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+require("../icons/money.svg");
+require("../icons/tag.svg");
+require("../icons/chart.svg");
+
+
 const NavWrapper = styled.nav`
     box-shadow:0 0 3px rgba(0,0,0,0.25);
     line-height: 24px;
@@ -11,6 +16,21 @@ const NavWrapper = styled.nav`
             width: 33.33%;
             text-align: center;
             padding: 16px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            > .item{
+                padding: 4px 0;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                > .icon{
+                    width: 24px;
+                    heigth: 24px;
+                }
+            }
         }
     }
 `
@@ -18,15 +38,33 @@ const Nav = ()=>{
     return (
         <NavWrapper>
         <ul>
-        <li><Link to="/tags">标签页</Link></li>
-        <li><Link to="/money">
-            <img src={x} alt="/"></img>
+        <li>
+            <Link to="/tags" className="item">  
+            <svg className="icon">
+                <use xlinkHref="#tag"/>
+            </svg>
+                标签页
+            </Link>
+            
+        </li>
+        <li>
+            <Link to="/money" className="item">
+            <svg className="icon">
+                <use xlinkHref="#money"/>
+            </svg>
             记账页
             </Link>
         </li>
-        <li><Link to="/statistics">统计页</Link></li>
-      </ul>
-      </NavWrapper>
+        <li>
+            <Link to="/statistics" className="item">
+            <svg className="icon">
+                <use xlinkHref="#chart"/>
+            </svg>
+            统计页
+            </Link>
+        </li>
+        </ul>
+        </NavWrapper>
     )
 }
 export default Nav;
