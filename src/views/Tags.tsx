c3f9b04bb0a151f05a69a24fd38aacc44d40b32a
yake go <cazser>
2020-10-14 08:26:30 +0800

    抽离useTags函数

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useTags } from "useTags";
 import Layout from "../components/Layout"
-const Tags = () => (
+const Tags = () => {
+  const {tags, setTags} = useTags();
+  return(
     <Layout>
       <h2>标签页面</h2>
     </Layout>
   )
+}
 
   export default Tags;
