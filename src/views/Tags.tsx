3132f93cc9e02efb6cadda5ed868e0c8c7c9b328
yake go <cazser>
2020-10-14 09:24:18 +0800

    tagList样式完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTags } from "useTags";
 import Layout from "../components/Layout"
 import styled from 'styled-components';
+import Icon from "components/Icon";
 
 
 const TagList = styled.ol`
@@ -10,19 +11,55 @@ const TagList = styled.ol`
   > li{
     border-bottom: 1px solid #d5d5d9;
     padding: 12px 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 `//
 
+const Button = styled.button`
+  font-size: 18px;
+  border: none; padding: 8px 12px;
+  background: #767676; border-radius: 4px;
+  color: white;
+`//
 
+const Center=styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`//
 
+const Space = styled.div`
+height: 16px;
+`//
 const Tags = () => {
   const {tags, setTags} = useTags();
+  const deleteTag = (tag: string)=>{
+    setTags(tags.filter(t => t!==tag))
+  }
   return(
     <Layout>
       <TagList>
         {tags.map(tag=>
-          <li key={tag}>{tag}</li>)}
+          {
+          return(
+          <li key={tag}>
+            <span>{tag}</span>
+          <div onClick={()=>deleteTag(tag)}>
+          <Icon name="delete"/>
+          </div>
+          </li>
+          );
+        }
+          )}
       </TagList>
+      <Center>
+        <Space/>
+        <Button>新增标签</Button>
+        <Space/>
+      </Center>
     </Layout>
   )
 }
